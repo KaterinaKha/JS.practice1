53bed4a31458dd4659d2c5ed483da697622c3b0b
KaterinaKha <katerinakhaa@gmail.com>
2023-04-04 15:23:21 +0200

    Update file.js

--- js/file.js
+++ js/file.js
@@ -1,4 +1,4 @@
-// "use strict";
+"use strict";
 
 // // Замовлення турів:
 // // Пишемо реєстрацію користувача за допомогою prompt.  Окремо логін та пароль. Валідацію не потрібно робити.
@@ -30,7 +30,7 @@
 
 // let maxPrice = prompt("Введіть суму, яку ви готові витратити на тур");
 
-// //----------------------------------------------------------------
+// // ----Практичне заняття 2 --------------------------------------------------
 // // Реалізуйте result
 // const a = ["a", "b", "c"];
 // const b = [1, [2, 3]];
@@ -81,99 +81,21 @@
 // 			if (firstArray === secondArray) commonElements.push(firstArray);
 // 		});
 // 	});
+// ---------------------------------------------------
 
-// 	return commonElements;
-// 	// Change code above this line
-// }
-// getCommonElements([1, 2, 3], [2, 4]);
-
-// const tweets = [
-//   { id: "000", likes: 5, tags: ["js", "nodejs"] },
-//   { id: "001", likes: 2, tags: ["html", "css"] },
-//   { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
-//   { id: "003", likes: 8, tags: ["css", "react"] },
-//   { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
-// ];
-
-// // Пройдемо по всіх елементах колекції і додамо значення властивості likes
-// // до акумулятора, початкове значення якого вкажемо 0.
-// const likes = tweets.reduce((totalLikes, tweet) => totalLikes + tweet.likes, 0);
-
-// console.log(likes); // 32
-
-// // Мабуть, підрахунок лайків - не одиночна операція, тому напишемо функцію
-// // для підрахунку лайків з колекції
-// const countLikes = tweets => {
-//   return tweets.reduce((totalLikes, tweet) => totalLikes + tweet.likes, 0);
+// const services = {
+//   showMenu() {},
+//   getMenu() {},
+//   addOrder() {},
+//   confirmOrder() {},
 // };
 
-// console.log(countLikes(tweets)); // 32
+// const torpedaDelivery = {
+//   order: [],
+//   chosenRestaurant: "",
+//   getAvailableRestaurants() {},
+//   chooseRestaurant() {},
+//   chooseDishes() {},
+// };
 
-// const tweets = [
-//   { id: "000", likes: 5, tags: ["js", "nodejs"] },
-//   { id: "001", likes: 2, tags: ["html", "css"] },
-//   { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
-//   { id: "003", likes: 8, tags: ["css", "react"] },
-//   { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
-// ];
-
-// // Пройдемо по всіх елементах колекції і додамо значення властивості tags
-// // до акумулятора, початкове значення якого вкажемо порожнім масивом [].
-// // На кожній ітерації пушимо в акумулятор усі елементи tweet.tags і повертаємо його.
-// const tags = tweets.reduce((allTags, tweet) => {
-//   allTags.push(...tweet.tags);
-
-//   return allTags;
-// }, []);
-
-// console.log(tags);
-
-// // Мабуть, збирання тегів - не одиночна операція, тому напишемо функцію
-// // для збирання тегів з колекції
-// const getTags = tweets =>
-//   tweets.reduce((allTags, tweet) => {
-//     allTags.push(...tweet.tags);
-
-//     return allTags;
-//   }, []);
-
-// console.log(getTags(tweets));
-
-const tweets = [
-	{ id: "000", likes: 5, tags: ["js", "nodejs"] },
-	{ id: "001", likes: 2, tags: ["html", "css"] },
-	{ id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
-	{ id: "003", likes: 8, tags: ["css", "react"] },
-	{ id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
-];
-
-const getTags = tweets =>
-	tweets.reduce((allTags, tweet) => {
-		allTags.push(...tweet.tags);
-
-		return allTags;
-	}, []);
-
-const tags = getTags(tweets);
-
-// Винесемо callback-функцію окремо, а в reducе передамо посилання на неї.
-// Це стандартна практика, якщо callback-функція досить велика.
-
-// Якщо в об'єкті-акумуляторі acc відсутня своя властивість з ключем tag,
-// то створюємо її і записуємо їй значення 0.
-// В іншому випадку збільшуємо значення на 1.
-const getTagStats = (acc, tag) => {
-	if (!acc.hasOwnProperty(tag)) {
-		acc[tag] = 0;
-	}
-
-	acc[tag] += 1;
-
-	return acc;
-};
-
-// Початкове значення акумулятора - це порожній об'єкт {}
-const countTags = tags => tags.reduce(getTagStats, {});
-
-const tagCount = countTags(tags);
-console.log(tagCount);
+// torpedaDelivery.chooseRestaurant();
